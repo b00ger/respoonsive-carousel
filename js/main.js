c2b37af8ceac6f5b87539fa3a41af476e6e5fa25
Daniel Nguyen - Macbook Air <dannguyen@me.com>
2014-03-29 14:02:37 -0500

    Added next/prev functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var RespoonsiveCarousel = function(options){
-	this.options = options;		
+	
+	this.mode = (typeof(options)!= "undefined") ? options.mode : "default";		
+	this.currentTile = 0;
 	this.carouselLength = $(".respoonsive-carousel ul.container > li").length;
 	this.tileWidth = $('.respoonsive-carousel').width();
 	this.containerWidth = this.carouselLength * this.tileWidth;
@@ -11,19 +13,33 @@ RespoonsiveCarousel.prototype.init = function(){
 	$('.respoonsive-carousel ul.container li').css('width' , $('.respoonsive-carousel').width() + 'px');
 	$(".respoonsive-carousel ul.container").css('width',this.containerWidth + 'px');
 	
-	if(this.options.mode == "thumbs"){
-		if($('.respoonsive-carousel-controls').length == 0){
-			$('.respoonsive-carousel').append('<ul class="respoonsive-carousel-controls"></ul>');
+	if(this.mode == "thumbs"){
+		if($('.respoonsive-carousel-status').length == 0){
+			$('.respoonsive-carousel').append('<ul class="respoonsive-carousel-status"/>');
 			for(var i=0 ; i < this.carouselLength; i++){
 				
-				$('.respoonsive-carousel-controls').append('<li><a href="#"' + (i==0 ? " class='active' " : "" ) +'>' + Number(i+1) + '</a></li>');
+				$('.respoonsive-carousel-status').append('<li><a href="#"' + (i==0 ? " class='active' " : "" ) +'>' + Number(i+1) + '</a></li>');
 			}
 		}
-		$('.respoonsive-carousel-controls li a').click(function(e){
+		$('.respoonsive-carousel-status li a').click(function(e){
 			e.preventDefault();
-			$('.respoonsive-carousel-controls li a').removeClass('active');
+			$('.respoonsive-carousel-status li a').removeClass('active');
 			$(this).addClass('active');
 			$(".respoonsive-carousel ul.container").animate({"left" : $(this).parent().index() * -1 * $this.tileWidth + "px"});
+		});
+	}else {
+		$('.respoonsive-carousel').append('<a class="prev">Prev</a><a class="next">Next</a>');
+		$('a.prev').click(function(e){
+			$this.currentTile = (($this.currentTile - 1) > 0) ? $this.currentTile - 1 : 0; 
+			e.preventDefault();
+			console.log($this.currentTile);
+			$(".respoonsive-carousel ul.container").animate({"left" : $this.currentTile * -1 * $this.tileWidth + "px"});
+		});
+		$('a.next').click(function(e){
+			$this.currentTile = (($this.currentTile + 1) < $this.carouselLength) ? $this.currentTile + 1 : 0; 
+			e.preventDefault();
+			console.log($this.carouselLength);
+			$(".respoonsive-carousel ul.container").animate({"left" : $this.currentTile * -1 * $this.tileWidth + "px"});
 		});
 	}
 	$(window).resize(function(){$this.resizeCarousel();})
@@ -31,12 +47,12 @@ RespoonsiveCarousel.prototype.init = function(){
 }
 RespoonsiveCarousel.prototype.resizeCarousel = function(){
 	$(".respoonsive-carousel ul.container").css('left' , '0px');
-	$('.respoonsive-carousel-controls li a').unbind('click');
+	$('.respoonsive-carousel-status li a').unbind('click');
 	$('.respoonsive-carousel ul.container li').css('width' , $('.respoonsive-carousel').width() + 'px');
 	$(".respoonsive-carousel ul.container").css('width',this.containerWidth + 'px');
-	$('.respoonsive-carousel-controls li a').click(function(e){
+	$('.respoonsive-carousel-status li a').click(function(e){
 		e.preventDefault();
-		$('.respoonsive-carousel-controls li a').removeClass('active');
+		$('.respoonsive-carousel-status li a').removeClass('active');
 		$(this).addClass('active');
 		$(".respoonsive-carousel ul.container").animate({"left" : $(this).parent().index() * -1 * $this.tileWidth + "px"});
 	});
